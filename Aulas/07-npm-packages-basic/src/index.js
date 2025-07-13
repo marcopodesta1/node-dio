f81b4ab67fd9c1d05fb7e0985a0a43644ca7fee6
marcopodesta1 <marcopodesta@hotmail.com>
2025-07-12 23:24:44 -0300

    commit aula 07-npm-packages-basic

---
+++ Aulas/07-npm-packages-basic/src/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(logSymbols.success, chalk.green.italic("servidor iniciado"));
+console.log(logSymbols.error, chalk.red.italic("servidor com problema"))
