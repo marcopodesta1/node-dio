176e4b57aa288dc7cb0869b30d73525b6e6cceb9
marcopodesta1 <marcopodesta@hotmail.com>
2025-08-15 00:08:21 -0300

    commit aula 09-npm-enviroment

---
+++ Aulas/09-npm-enviroment/src/database/data.js
@@ -0,0 +1,8 @@
+async function connectToDatabase(user, password) {
+    if (user === process.env.USERDATABASE && password === process.env.PASSWORDDATABASE)
+        console.log("conexão com banco de dados estabelecida");
+    else
+        console.log("Falha de login, não foi posível se conectar ao banco de dados");
+}
+
+export default connectToDatabase
---
+++ Aulas/09-npm-enviroment/src/index.js
@@ -0,0 +1,7 @@
+import connectToDatabase from "./database/data.js";
+
+async function main() {
+    await connectToDatabase(process.env.USERDATABASE, process.env.PASSWORDDATABASE);
+}
+
+main();
