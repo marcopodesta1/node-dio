71e16f0826faf5b437654d12c341338095de5aa7
marcopodesta1 <marcopodesta@hotmail.com>
2025-08-30 12:58:46 -0300

    commit aula 10-typescript

---
+++ Aulas/10-typescript/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "ol\xE1 felip\xE3o";
+  console.log(myEmail);
+}
+main();
---
+++ Aulas/10-typescript/src/index.ts
@@ -0,0 +1,6 @@
+function main() {
+  let myEmail: string = "olá felipão";
+  console.log(myEmail);
+}
+
+main();
